d6eec646339fb666bee7def12da8d6ba0711f3d5
nenuchepanu001 <137167809+nenuchepanu001@users.noreply.github.com>
2024-06-03 17:23:49 +0530

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 # ------------------------------------------------
-# Author       : KS
+# Author       : Krishna Sai
 # Created Date : 03/06/2024
 # LastModified : 03/06/2024
-# LastAuthor   : KS
+# LastAuthor   : Krishna Sai
 # Description
 # to create back up script
 # Usage
-# backup_script
+# our_first_script
 # -----------------------------------------------
+echo "Hello, ${USER^}"
+echo ""
 
-tar -cvf ~/bash_course/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
+echo "I will now backup your home directory, $HOME"
+echo ""
 
+currentdir=$(pwd)
+echo "you are running this script form $currentdir"
+echo ""
+
+echo "Therefore, I will save the backup in $currentdir"
+echo ""
+
+tar -cvf $currentdir/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar $HOME/bash_course/* 2>/dev$
+
+# -v (verbose) in tar will hide the echo commands
 echo "backup created successfully"
 exit 0
