6193d113ebfa74d2c43f467ef0606037a3966a6e
nenuchepanu001 <137167809+nenuchepanu001@users.noreply.github.com>
2024-06-03 12:34:48 +0530

    Create variable_practise.sh

---
+++ variable_practise.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# -----------------------------------------------
+# Author       : KS
+# Created Date : 03/06/2024
+# LastModified : 03/06/2024
+# LastAuthor   : KS
+#                                                                                        $
+# Description
+# variable practise how we assign and see in console
+#                                                                                        $
+# Usage
+# variable_practise
+# -----------------------------------------------
+
+student="Sarah"
+
+#no spaces around equalsign when you work with bash
+#if you give spaces it will think as  a command
+echo "Hello ${student}"
+
+exit 0
